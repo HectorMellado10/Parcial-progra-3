e6a99d8a624065da93f9bd85cd876480491bddea
Hector Mellado <apixelado07@gmail.com>
2024-03-01 22:08:53 -0600

    Agregar archivos de proyecto.

---
+++ Parcial progra 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Tabla de Multiplicar ---");
+
+        int numeroTabla;
+
+        Console.Write("Ingrese un número para imprimir la tabla de multiplicar: ");
+        while (!int.TryParse(Console.ReadLine(), out numeroTabla))
+        {
+            Console.Write("Ingrese un número entero válido: ");
+        }
+
+        ImprimirTablaMultiplicar(numeroTabla);
+
+        Console.WriteLine("\nFin del programa.");
+    }
+
+    static void ImprimirTablaMultiplicar(int numero)
+    {
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}
